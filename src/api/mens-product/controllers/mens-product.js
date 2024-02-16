e9e84cd4316d750f4660fbc03fcf668641a8fdb1
Siddharth Gupta <siddharth11tha@gmail.com>
2024-02-16 17:07:41 +0530

    intial commit

---
+++ src/api/mens-product/controllers/mens-product.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * mens-product controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::mens-product.mens-product');
---
+++ src/api/mens-product/services/mens-product.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * mens-product service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::mens-product.mens-product');
